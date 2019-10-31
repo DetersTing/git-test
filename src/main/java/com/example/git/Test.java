5e1b9131665bacd98ec3f15c4c1cd15045053850
zhuluqing <zhuluqing@lzt.com>
2019-10-31 10:44:11 +0800

    git2进行修改

--- src/main/java/com/example/git/Test.java
+++ src/main/java/com/example/git/Test.java
@@ -8,5 +8,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git1-update");
         System.out.println("git2-update");
+
+        System.out.println("git2-update2");
     }
 }
